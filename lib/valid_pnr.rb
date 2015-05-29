d8a657ea721f7fa0a131828048f6d978826c5101
Islam Alchasov <islam.alchasov@elev.it-gymnasiet.se>
2015-05-29 15:51:07 +0200

    Done
    Haven't tested rspec, due to not using a mac.

--- lib/valid_pnr.rb
+++ lib/valid_pnr.rb
@@ -1,9 +1,30 @@
+def valid_pnr?(pnr:)
+  pnr = pnr.split('').map { |x| x.to_i }
+  pnr.delete_at(6)
 
-def valid_pnr?(pnr)
-  i=2
-  nr=0
+  checksum = pnr.pop
+  total = 0
 
-  p pnr
+  pnr.each_with_index do |digit,index |
+    if index % 2 == 0
+      val = digit * 2
+      if val > 9
+        val = val - 9
+      end
+    else
+      val = digit
+    end
+    total += val
+  end
+
+  checking = total + checksum
+
+  if checking % 10 == 0
+    return true
+  elsif checking % 10 != 0
+    return false
+  else
+    return 'Error'
+  end
+
 end
-
-valid_pnr?(pnr: 1234123)
